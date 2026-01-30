89ad6065b8e21532fb7b08db6aab12e63814990d
Kuan Fan <kuan.fan@gov.bc.ca>
2026-01-30 15:35:16 -0800

    chore: restore default release rules

--- .releaserc.rc.js
+++ .releaserc.rc.js
@@ -1,12 +1,7 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        releaseRules: [{ type: "chore", release: "patch" }],
-      },
-    ],
+    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     [
